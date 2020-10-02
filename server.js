97758a6900a74a990d986dcf78579a592813006c
Axel Hamilton <axelhamilton02@gmail.com>
2020-10-02 09:04:51 +0000

    Définition des variables d'environnement

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const redis = require('redis');
 const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const redisStore = require('connect-redis')(session);
 
 const app = express();
+const client = redis.createClient();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(session({secret:'iloveyou2002', saveUninitialized:true, resave:true}))
+app.use(session({
+    secret:process.env.SECRET,
+    store: new redisStore({host: 'localhost', port: 6379, client: client, ttl: 260}),
+    saveUninitialized:false,
+    resave:false
+}))
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'static')));
 
@@ -25,6 +33,7 @@ app.use(mdpforgot);
 app.use(changemdp);
 
 // Listen on port 5000
-httpServer.listen(5000, ()=>{
+let port = process.env.PORT;
+httpServer.listen(port, ()=>{
     console.log('Server starting on port 5000');
 });
